4a9b1c5b7cd817973c1eb8a0291716568232edce
Cezar Neaga <hi@cezarneaga.eu>
2020-11-20 15:43:17 +0100

    fix #3 remove scroll

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,11 @@ import { Flex } from '@chakra-ui/core'
 
 export function Main({ children }) {
   return (
-    <Flex minHeight="780px" width="1400px" mx="auto" my="100px">
+    <Flex
+      minHeight="780px"
+      width="1400px"
+      mx="auto"
+      my="calc((100vh - 985px) / 2)">
       {children}
     </Flex>
   )
